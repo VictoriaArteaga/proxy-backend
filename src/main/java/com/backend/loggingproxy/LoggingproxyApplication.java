aa0dfb8cffa2e195a64bd833a73b080c8c1bfcb2
María Victoria <maria.arteagare@campusucc.edu.co>
2026-04-17 14:37:07 -0500

    Configuración inicial

---
+++ src/main/java/com/backend/loggingproxy/LoggingproxyApplication.java
@@ -0,0 +1,13 @@
+package com.backend.loggingproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingproxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/loggingproxy/LoggingproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.loggingproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
